414b837f5a9adb8726f395d66bd634acb427837b
Pavel Peev <76268971+thesquashedman@users.noreply.github.com>
2022-12-01 14:51:07 -0800

    Normals now recomputed for cylinder at 360 degrees

--- Assets/Scripts/CylinderMesh.cs
+++ Assets/Scripts/CylinderMesh.cs
@@ -25,6 +25,7 @@ public class CylinderMesh : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        
         Mesh theMesh = GetComponent<MeshFilter>().mesh;
         Vector3[] v = theMesh.vertices;
         Vector3[] normal = theMesh.normals;
@@ -37,11 +38,28 @@ public class CylinderMesh : MonoBehaviour
 
         theMesh.vertices = v;
         theMesh.normals = normal;
+        
     }
     Vector3[] MakeVertices(ref Vector3[] normalArray)
     {
         Vector3[] vertexArray = new Vector3[(upResolution + 1) * (resolution + 1)];
         for(int i = 0; i < resolution + 1; i++)
+        {
+            for(int j = 0; j < upResolution + 1; j++)
+            {
+                Vector3 xzPosition = new Vector3(radius * Mathf.Cos(i * (Mathf.Deg2Rad * degrees/resolution)), 0, radius * Mathf.Sin(i * (Mathf.Deg2Rad * degrees/resolution)));
+                Vector3 yPosition = new Vector3(0, j * squareHeight, 0);
+                vertexArray[j + i * (upResolution + 1)] = xzPosition + yPosition;
+                //Debug.Log(vertexArray[j + i * (upResolution + 1)]);
+                normalArray[j + i * (upResolution + 1)] = xzPosition.normalized;
+            }
+        }
+        return vertexArray;
+    }
+    Vector3[] MakeVertices360(ref Vector3[] normalArray)
+    {
+        Vector3[] vertexArray = new Vector3[(upResolution + 1) * (resolution)];
+        for(int i = 0; i < resolution; i++)
         {
             for(int j = 0; j < upResolution + 1; j++)
             {
@@ -83,13 +101,65 @@ public class CylinderMesh : MonoBehaviour
         }
         return triangleArray;
     }
+    int[] MakeTriangles360()
+    {
+        /*
+        for(int i = 0; i < vertexArray.Length; i += 2)
+        {
+            vertexArray[i] = point1 + new Vector3(0, i * squareHeight, 0);
+            vertexArray[i+1] = point2 + new Vector3(0, i * squareHeight, 0); 
+        }
+        */
+        int[] triangleArray = new int[(upResolution * resolution) * 2 * 3];
+
+        for(int j = 0; j < resolution - 1; j++)
+        {
+
+        
+            for(int i = 0; i < upResolution; i++)
+            {
+                triangleArray[i * 6 + j * (upResolution) * 6] = 0 + i + j * (upResolution + 1);
+                triangleArray[i * 6 + j * (upResolution) * 6 + 1] = 1 + i + j * (upResolution + 1);
+                triangleArray[i * 6 + j * (upResolution)* 6 + 2] = 1 + i + (j + 1) * (upResolution + 1);
+
+                triangleArray[i * 6 + j * (upResolution)* 6 + 3] = 0 + i + j * (upResolution + 1);
+                triangleArray[i * 6 + j * (upResolution)* 6 + 4] = 1 + i + (j + 1) * (upResolution + 1);
+                triangleArray[i * 6 + j * (upResolution) * 6 + 5] = 0 + i + (j + 1) * (upResolution + 1);
+                
+            }
+        }
+        for(int i = 0; i < upResolution; i++)
+            {
+                triangleArray[i * 6 + (resolution - 1) * (upResolution) * 6] = 0 + i + (resolution - 1) * (upResolution + 1);
+                triangleArray[i * 6 + (resolution - 1) * (upResolution) * 6 + 1] = 1 + i + (resolution - 1) * (upResolution + 1);
+                triangleArray[i * 6 + (resolution - 1) * (upResolution)* 6 + 2] = 1 + i;
+
+                triangleArray[i * 6 + (resolution - 1) * (upResolution)* 6 + 3] = 0 + i + (resolution - 1) * (upResolution + 1);
+                triangleArray[i * 6 + (resolution - 1) * (upResolution)* 6 + 4] = 1 + i;
+                triangleArray[i * 6 + (resolution - 1) * (upResolution) * 6 + 5] = 0 + i;
+                
+            }
+        return triangleArray;
+    }
     void ChangeResolution()
     {
         Mesh theMesh = GetComponent<MeshFilter>().mesh;  
         theMesh.Clear();
-        Vector3[] normalArray = new Vector3[(upResolution + 1) * (resolution + 1)];
-        Vector3[] vertexArray = MakeVertices(ref normalArray);
-        int[] triangleArray = MakeTriangles();
+        Vector3[] normalArray;
+        Vector3[] vertexArray;
+        int[] triangleArray;
+        if(degrees != 360)
+        {
+            normalArray = new Vector3[(upResolution + 1) * (resolution + 1)];
+            vertexArray = MakeVertices(ref normalArray);
+            triangleArray = MakeTriangles();
+        }
+        else
+        {
+            normalArray = new Vector3[(upResolution + 1) * (resolution)];
+            vertexArray = MakeVertices360(ref normalArray);
+            triangleArray = MakeTriangles360();
+        }
         //MakeTriangles();
         theMesh.vertices = vertexArray;
         theMesh.triangles = triangleArray;
@@ -141,19 +211,40 @@ public class CylinderMesh : MonoBehaviour
 
     void ComputeNormals(Vector3[] v, Vector3[] normal,int m, int n)
     {
-        int[] temp = MakeTriangles();
+        int[] temp;
+        if(degrees != 360)
+        {
+            temp = MakeTriangles();
+        }
+        else
+        {
+            temp = MakeTriangles360();
+        }
         Vector3[] triNormal = new Vector3[m * n * 2];
         for (int i = 0; i < triNormal.Length; i++)
         {
             triNormal[i] = FaceNormal(v, temp[0 + 3 * i], temp[1 + 3 * i], temp[2 + 3 * i]);
         }
-        for(int i = 0; i < normal.Length; i++)
+        if(degrees != 360)
         {
-            
-            normal[i] = TrianglesAround(i, m, n, triNormal);
-            
-            
-            
+            //Debug.Log("not 360");
+            for(int i = 0; i < normal.Length; i++)
+            {
+                
+                normal[i] = TrianglesAround(i, m, n, triNormal);
+                
+                
+                
+            }
+        }
+        else
+        {
+            for(int i = 0; i < normal.Length; i++)
+            {
+                
+                normal[i] = TrianglesAround360(i, m, n, triNormal);
+                
+            }
         }
         /*
         triNormal[0] = FaceNormal(v, 3, 4, 0);
@@ -178,6 +269,95 @@ public class CylinderMesh : MonoBehaviour
         normal[8] = (triNormal[6] + triNormal[7]).normalized;
         */
         UpdateNormals(v, normal);
+    }
+    Vector3 TrianglesAround360(int i, int m, int n, Vector3[] triNormal)
+    {
+        int row = i % (n+1);
+        int column = i / (n+1);
+
+        if(row == 0)
+        {
+            if(column == 0)
+            {
+                int tri1 = 0;
+                int tri2 = 1;
+                int tri3 = (n * 2) * (m - 1) + 1;
+                return (triNormal[tri1] + triNormal[tri2] + triNormal[tri3]).normalized;
+            }
+            /*
+            else if(column == m)
+            {
+                int tri1 = (n * 2) * (m - 1) + 1;
+                int tri2 = 0;
+                int tri3 = 1;
+                return (triNormal[tri1] + triNormal[tri2] + triNormal[tri3]).normalized;
+            }
+            */
+            else
+            {
+                int tri1 = (column - 1) * (n * 2) + 1;
+                int tri2 = tri1 + n*2;
+                int tri3 = tri2 - 1;
+                return (triNormal[tri1] + triNormal[tri2] + triNormal[tri3]).normalized;
+            }
+        }
+        else if(row == n)
+        {
+            if(column == 0)
+            {
+                int tri1 = (n - 1) * 2;
+                int tri2 = n * 2 * (m - 1) + (n - 1) * 2;
+                int tri3 = tri2 + 1;
+                return (triNormal[tri1] + triNormal[tri2] + triNormal[tri3]).normalized;
+            }
+            /*
+            else if(column == m)
+            {
+                int tri1 = n * 2 * (m - 1) + (n - 1) * 2;
+                int tri2 = tri1 + 1;
+                int tri3 = (n - 1) * 2;
+                return (triNormal[tri1] + triNormal[tri2] + triNormal[tri3]).normalized;
+            }
+            */
+            else
+            {
+                int tri1 = (row - 1) * 2 + (column - 1) * n * 2 + 1;
+                int tri2 = tri1 + 1;
+                int tri3 = tri1 + n*2;
+                return (triNormal[tri1] + triNormal[tri2] + triNormal[tri3]).normalized;
+            }
+        }
+        else if(column == 0 && row != 0 && row != n)
+        {
+            int tri1 = (row - 1) * 2;
+            int tri2 = tri1 + 1;
+            int tri3 = tri2 + 2;
+            int tri4 = (row - 1) * 2 + (m - 1) * n * 2;
+            int tri5 = tri4 + 1;
+            int tri6 = tri5 + 2;
+            return (triNormal[tri1] + triNormal[tri2] + triNormal[tri3] + triNormal[tri4] + triNormal[tri5] + triNormal[tri6]).normalized;
+        }
+        /*
+        else if(column == m && row != 0 && row != n)
+        {
+            int tri1 = (row - 1) * 2 + (column - 1) * n * 2;
+            int tri2 = tri1 + 1;
+            int tri3 = tri2 + 2;
+            return (triNormal[tri1] + triNormal[tri2] + triNormal[tri3]).normalized;
+        }
+        */
+        //Triangles for when point is in the middle
+        else
+        {
+            int tri1 = (row - 1) * 2 + (column - 1) * n * 2; //Bot left square top triangle
+            int tri2 = tri1 + 1;
+            int tri3 = tri2 + 1;
+            int tri4 = tri2 + n * 2; //Top left square top triangle
+            int tri5 = tri4 + 1;
+            int tri6 = tri5 + 1;
+            return (triNormal[tri1] + triNormal[tri2] + triNormal[tri3] + triNormal[tri4] + triNormal[tri5] + triNormal[tri6]).normalized;
+        }
+
     }
     Vector3 TrianglesAround(int i, int m, int n, Vector3[] triNormal)
     {
